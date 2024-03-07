dec94017d66ff599001d0c858da0d2c15ee88199
Julian Duque Tabares <julianduque85@gmail.com>
2024-03-07 12:11:27 -0500

    Componentes en Angular, Uso de Inputs

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  //img = 'https://picsum.photos/640/640?r=' + Math.random();
+  @Input({required: true}) img = '';
+  @Input({required: true}) title = '';
+  @Input({required: true}) price = 0;
+}
