a7772328d75217136a838a4a95886fc48f980797
Julian Duque Tabares <julianduque85@gmail.com>
2024-03-20 18:41:22 -0500

    page not found, routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { ListComponent } from '@products/pages/list/list.component'
 import { AboutComponent } from '@info/pages/about/about.component'
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: '**', component: NotFoundComponent}
 ];
