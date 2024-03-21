93507452f27772a7357a410a7b99a57f55da0f93
Julian Duque Tabares <julianduque85@gmail.com>
2024-03-21 14:35:38 -0500

    Mostrando categorias desde la API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,8 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 
 import { ProductComponent } from '@products/components/product/product.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
+import { Category } from '@shared/models/category.model';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -16,12 +18,23 @@ import { ProductService } from '@shared/services/product.service';
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   constructor() {}
 
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts().subscribe({
       next: (products) => {
         this.products.set(products);
@@ -32,7 +45,14 @@ export class ListComponent {
     })
   }
 
-  addToCart(product: Product){
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: () => {
+        console.error('Error');
+      }
+    })
   }
 }
